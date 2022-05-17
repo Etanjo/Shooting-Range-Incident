1a93bbcdebaf98d6709cc93958a2742c0566e70d
Andrew Kelley <99751328+BiggieCheeser@users.noreply.github.com>
2022-05-17 16:13:45 +0000

    jklt;

--- anrrews work/movement.ts
+++ anrrews work/movement.ts
@@ -1,4 +1,3 @@
-//fcanvas and fctx will be replaced by ethan's canvas names
 import alphaUrl from './jacksoon/alpha.png';
 
 let assetDiv : HTMLDivElement = document.querySelector('#jacksoon');
@@ -6,13 +5,24 @@ let assetDiv : HTMLDivElement = document.querySelector('#jacksoon');
 export let alphaImage = document.createElement('img');
 alphaImage.src = alphaUrl;
 assetDiv.appendChild(alphaImage);
-  
+
+//assetDiv.style.display = 'none';
+
+
+
+export let alpha = {
+  x: 100,
+  y: 100,
+  s: 100,
+  image: alphaImage,
+};
+
 function drawalpha () {
-  let fctx = fcanvas.getContext('2d');
-  fctx.resetTransform();
-  fctx.clearRect(0,0,fcanvas.width,fcanvas.height);
-  fctx.translate(alpha.x+25 , alpha.y+25)
-  fctx.drawImage(
+  let pctx = pcanvas.getContext('2d');
+  pctx.resetTransform();
+  pctx.clearRect(0,0,pcanvas.width,pcanvas.height);
+  pctx.translate(alpha.x+25 , alpha.y+25)
+  pctx.drawImage(
     alphaImage,
     -100,
     -100,
@@ -21,7 +31,32 @@ function drawalpha () {
   )
 }
 
-function drawEnemy1(){
-  let ctx = canvas.getContext('2d');
-  
+window.addEventListener(
+  'keydown',
+  function (event) {
+    console.log(event.key,'pressed');
+    if (event.key == 'ArrowDown' || event.key=='s') {
+      alpha.y += 20;
+    }
+    if (event.key == 'ArrowUp' || event.key=='w') {
+      alpha.y -= 20;
+    }
+  }
+)
+
+let div = document.querySelector("div");
+
+let frame = 0;
+
+let nframes = 22;
+let size = 32;
+function showNextFrame(){
+  frame = (frame +1)% nframes;
+  let offset = frame * size
+  div.style.backgroundPosition = `-${offset}px`;
 }
+
+setInterval(
+  showNextFrame,
+  1000/6
+);
