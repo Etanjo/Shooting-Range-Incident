11bfe3ca99d4bb46d2dbf1b347de1503e72424d7
Etanjo <99751108+Etanjo@users.noreply.github.com>
2022-05-17 16:25:28 +0000

    Added player canvas

--- earthan/canvas.ts
+++ earthan/canvas.ts
@@ -1,10 +1,10 @@
 export let app = document.querySelector('#app');
 
-export let playerCanvas = document.createElement('canvas');
+export let pCanvas = document.createElement('canvas');
 export let bgCanvas = document.createElement('canvas');
 
 app.appendChild(bgCanvas);
-app.appendChild(playerCanvas);
+app.appendChild(pCanvas);
 
 export function setupCanvas(canvas:HTMLCanvasElement){
 canvas.width = 1500;
@@ -20,3 +20,11 @@ canvas.style.borderStyle = 'solid';
 
 setupCanvas(bgCanvas)
 export let bgctx = bgCanvas.getContext('2d')
+export let pctx = pCanvas.getContext('2d')
+pCanvas.style.width = '375px'
+pCanvas.style.height = '650px'
+pCanvas.height = 650
+pCanvas.width = 375
+pCanvas.style.borderWidth = '3px';
+pCanvas.style.borderColor = 'Black';
+pCanvas.style.borderStyle = 'solid';
