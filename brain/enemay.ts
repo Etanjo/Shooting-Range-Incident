8a52faab41a89744995c7548f7eb5c5a119d2377
Brian Wagner <99751146+IACSstudent1@users.noreply.github.com>
2022-05-17 16:23:51 +0000

    briked'

---
+++ brain/enemay.ts
@@ -0,0 +1,31 @@
+/*
+import greenImage from './jacksoon/green.png';
+
+
+function drawsolder () {
+  let pctx = bganvas.getContext('2d');
+  pctx.resetTransform();
+  pctx.clearRect(0,0,bganvas.width,bganvas.height);
+  pctx.translate(green.x+25 , green.y+25)
+  pctx.drawImage(
+    greenImage,
+    -100,
+    -100,
+    200,
+    200
+  )
+}
+
+
+export let green = {
+  x: 100,
+  y: 100,
+  s: 100,
+  image: greenImage,
+};
+
+greenImage.addEventListener(
+  "load",
+function () {animatesword()}
+);
+*/
