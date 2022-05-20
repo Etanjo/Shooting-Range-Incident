11795c429c43c22208d89111ddd259d369da1b09
Etanjo <99751108+Etanjo@users.noreply.github.com>
2022-05-20 16:10:51 +0000

    Added UI canvas and Bullet sprite

---
+++ earthan/bullets.ts
@@ -0,0 +1,17 @@
+/*import {bCanvas} from './canvas'
+import {bctx} from './canvas'
+import {bulletUrl} from '/earthan/bullet.png';
+let assetDiv : HTMLDivElement = document.querySelector('#assets');
+
+assetDiv.style.display = 'none'; // hide the images
+
+export let bulletImage = document.createElement('img');
+bulletImage.src = bulletUrl;
+assetDiv.appendChild(bulletImage);
+
+function animateBullet(x,y){
+  bctx.clearRect(0,0,bCanvas.width, bCanvas.height)
+  bctx.drawImage(bulletImage, x, y)
+  
+  
+}*/
--- earthan/canvas.ts
+++ earthan/canvas.ts
@@ -3,10 +3,12 @@ export let app = document.querySelector('#app');
 export let pCanvas = document.createElement('canvas');
 export let bgCanvas = document.createElement('canvas');
 export let bCanvas = document.createElement('canvas')
+export let uiCanvas = document.createElement('canvas')
 
 app.appendChild(bgCanvas);
 app.appendChild(pCanvas);
-app.appendChild(bCanvas)
+app.appendChild(bCanvas);
+app.appendChild(uiCanvas);
 
 export function setupCanvas(canvas:HTMLCanvasElement){
 canvas.width = 1500;
@@ -23,6 +25,7 @@ canvas.style.borderStyle = 'solid';
 setupCanvas(bgCanvas)
 export let bgctx = bgCanvas.getContext('2d')
 export let pctx = pCanvas.getContext('2d')
+export let bctx = bCanvas.getContext('2d')
 pCanvas.style.width = '375px'
 pCanvas.style.height = '650px'
 pCanvas.height = 650
@@ -38,3 +41,7 @@ setupCanvas(bCanvas)
 bCanvas.style.position = "absolute"
 bCanvas.style.right = '5px'
 bCanvas.style.top = '60px'
+setupCanvas(uiCanvas)
+uiCanvas.style.position = "absolute"
+uiCanvas.style.right = '5px'
+uiCanvas.style.top = '60px'
