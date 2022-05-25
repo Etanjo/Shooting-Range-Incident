6aa9bcd9595b26585ac05f90ffad1fdcf1836e74
Andrew Kelley <99751328+BiggieCheeser@users.noreply.github.com>
2022-05-25 14:59:28 +0000

    he exists

--- anrrew/movement.ts
+++ anrrew/movement.ts
@@ -1,6 +1,8 @@
 
-import alphaUrl from '/jacksoon/alpha.png';
+//import alphaUrl from '/jacksoon/alpha.png';
 
+import alphaUrl from '/anrrew/alphatm.png'
+  
 let assetDiv : HTMLDivElement = document.querySelector('#assets');
 
 export let alphaImage = document.createElement('img');
@@ -18,77 +20,7 @@ export let bartenderImage = document.createElement('img');
 bartenderImage.src = bartenderUrl;
 assetDiv.appendChild(bartenderImage)
 
-export let bartender = {
-  x:100,
-  y:100,
-  s:100,
-  image: bartenderImage
-}
-
 import {pCanvas} from '/earthan/canvas.ts'
-
-function drawBartender () {
-  let pctx = pCanvas.getContext('2d');
-   pctx.resetTransform();
- pctx.clearRect(0,0,pCanvas.width,pCanvas.height);
-  pctx.translate(bartender.x+25 , bartender.y+25)
-  pctx.drawImage(
-    bartenderImage,
-    -100,
-    -100,
-    200,
-    200
-  )
-}
-function updatefallingguy (elapsed : number) {
-  const fallSpeed = 10;
-  bartender.y += 1200 * elapsed/1000;
-  if (bartender.y > pCanvas.height +50 ){
-    bartender.y = 0;
-  }
-  if (bartender.x < 0){
-    bartender.x = pCanvas.width;
-  }
-  if (bartender.x >1000){
-    bartender.x = 0;
-  }
-}
-
-function animateBartender (timestamp : number = 0) {
-  let elapsed
-  if (timestamp){
-    if (!startTime){
-      startTime = timestamp;
-      elapsed = 0;
-    } else {
-      elapsed = timestamp - startTime
-      startTime = timestamp;
-    }
-    (elapsed);
-   // updateBartender(elapsed);
-  }
-  drawBartender();
-  requestAnimationFrame(animateBartender)
-};
-
-window.addEventListener(
-  'keydown',
-  function (event) {
-    console.log(event.key,'pressed');
-    if (event.key == 'ArrowDown' || event.key=='s') {
-      bartender.y += 20;
-    }
-    if (event.key == 'ArrowUp' || event.key=='w') {
-      bartender.y -= 20;
-    }
-  }
-)
-
-bartenderImage.addEventListener(
-  "load",
-  function () {drawBartender()}
-)
-
 
 export let alpha = {
   x: 100,
@@ -99,17 +31,47 @@ export let alpha = {
 
 function drawalpha () {
   let pctx = pCanvas.getContext('2d');
-  pctx.resetTransform();
-  pctx.clearRect(0,0,pCanvas.width,pCanvas.height);
+pctx.resetTransform();
+pctx.clearRect(0,0,pCanvas.width,pCanvas.height);
   pctx.translate(alpha.x+25 , alpha.y+25)
   pctx.drawImage(
     alphaImage,
-    -100,
-    -100,
+    alpha.x,
+    alpha.y,
     200,
     200
   )
 }
+
+function updatealpha (elapsed : number) {
+  alpha.y += 1200 * elapsed/1000;
+  if (alpha.y < 0){
+    alpha.y = 325;
+  }
+  if (alpha.x < 0){
+    alpha.x = 250;
+  }
+  if (alpha.x >250){
+    alpha.x = 0;
+  }
+}
+
+function animatealpha (timestamp : number = 0) {
+  let elapsed
+  if (timestamp){
+    if (!startTime){
+      startTime = timestamp;
+      elapsed = 0;
+    } else {
+      elapsed = timestamp - startTime
+      startTime = timestamp;
+    }
+    (elapsed);
+    updatealpha(elapsed);
+  }
+  drawalpha();
+  requestAnimationFrame(animatealpha)
+};
 
 window.addEventListener(
   'keydown',
@@ -123,7 +85,9 @@ window.addEventListener(
     }
   }
 )
+animatealpha()
 
+/*
 let div = document.querySelector("div");
 
 let frame = 0;
@@ -140,3 +104,4 @@ setInterval(
   showNextFrame,
   1000/6
 );
+*/
