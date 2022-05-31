94dfd6e159f9eafaaba734b820e58d23c7938a23
AndrewKelley2 <99751328+BiggieCheeser@users.noreply.github.com>
2022-05-31 15:50:21 +0000

    hahahahahahaahahahaha

--- anrrew/getshot.ts
+++ anrrew/getshot.ts
@@ -1,3 +1,15 @@
-import laser from './earthan/bullets.ts'
+import {lasers} from '/earthan/bullets.ts';
+import {green} from '/brain/enemay.ts';
 
+export function laserkill (laser) {
+  let distance = Math.sqrt(
+    (laser.x-green.y)*(laser.x-green.y)
+    +
+    (laser.y-green.y)*(laser.y-green.y)
+  );
+  console.log(distance);
+  if(distance <=50)
+  {green.x= Math.random()*1000}
+}
 
+laserkill();
