b49a569b821a595f1a325f11dc53db4af03d6917
Andrew Kelley <99751328+BiggieCheeser@users.noreply.github.com>
2022-06-07 16:14:03 +0000

    hhnhnbnbnfh

--- brain/enemay.ts
+++ brain/enemay.ts
@@ -25,30 +25,31 @@ function drawgreenImage() {
     200
   );*/
   ectx.drawImage(
-      green.image,  // image to draw
-      // offset is equal to 0
-      green.offset, 0,  // source offset
-      256, 256,  // source size
-      green.x, green.y, // destination offset
-      128, 128 // destination size
-      );    
+    green.image,  // image to draw
+    // offset is equal to 0
+    green.offset, 0,  // source offset
+    256, 256,  // source size
+    green.x, green.y, // destination offset
+    128, 128 // destination size
+  );
 }
 
 
 
 
 export let green = {
+
   x: 100,
   y: 100,
   s: 100,
-  offset : 0,
+  offset: 0,
   image: greenImage,
 };
 
 setInterval(
-  function () {
+  function() {
     // rotate between 0, 64, 128, 196, etc
-    green.offset = (green.offset + 256) % (256*4);
+    green.offset = (green.offset + 256) % (256 * 4);
   },
   180 // ms
 )
@@ -81,15 +82,15 @@ export function updategreen(elapsed: number) {
   green.x += 100 * elapsed / 1000
   if (green.x > 1000) {
     green.x = 0;
-    green.y = Math.random()*(650-128)
+    green.y = Math.random() * (650 - 128)
   }
-  
+
 }
 
 
 /*
 function let () {
-  
+
 }
 /*
 function animateCanvas() {
