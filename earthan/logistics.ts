3e43fb3d039dcfdac489a258974d3a2581416eca
Etanjo <99751108+Etanjo@users.noreply.github.com>
2022-06-07 15:45:09 +0000

    potential fix for netlify image loads, damage now affects kill chance

--- earthan/logistics.ts
+++ earthan/logistics.ts
@@ -1,8 +1,64 @@
 import {uiCanvas, uictx} from "./canvas"
+import rangeUrl from "/earthan/range.png"
+import woodUrl from '/earthan/wood.png'
+import AlphastillUrl from '/jacksoon/Alphastill.png'
+import laserUrl from '/earthan/laser.png'
+import bulletUrl from '/earthan/bullet.png'
+import solderUrl from '/jacksoon/solder.png'
+import JuggernautUrl from '/jacksoon/Juggernaut.png'
+import DreadnoughtUrl from '/jacksoon/Dreadnought.png'
+
+let range = rangeUrl
+let wood = woodUrl
+let alpha1 = AlphastillUrl
+let laser = laserUrl
+let bullet = bulletUrl
+let soldier = solderUrl
+let jug = JuggernautUrl
+let dread = DreadnoughtUrl
+let assetDiv = document.querySelector('#assets')
+
+function addToAssets(asset){
+  assetDiv.appendChild(asset)
+}
+
+addToAssets(range)
+addToAssets(wood)
+addToAssets(alpha1)
+addToAssets(laser)
+addToAssets(bullet)
+addToAssets(soldier)
+addToAssets(jug)
+addToAssets(dread)
+
+export let game = {
+  loading : true,
+  state : 1, //game states: 1 means it hasn't started, 2 is mid-game, and 3 is post-game
+  loads: 10, // loads = how many things need to load
+  
+}
+
+range.addEventListener('load', function(event){game.loads -= 1})
+wood.addEventListener('load', function(event){game.loads -= 1})
+alpha1.addEventListener('load', function(event){game.loads -= 1})
+laser.addEventListener('load', function(event){game.loads -= 1})
+bullet.addEventListener('load', function(event){game.loads -= 1})
+soldier.addEventListener('load', function(event){game.loads -= 1})
+jug.addEventListener('load', function(event){game.loads -= 1})
+dread.addEventListener('load', function(event){game.loads -= 1})
+  
 
 
 function drawLoad(){
-  
+  uictx.beginPath
+  uictx.fillStyle = 'black'
+  uictx.fillRect(0,0,uiCanvas.width,uiCanvas.height)
+  uictx.stroke()
+  uictx.beginPath
+  uictx.fillStyle = 'DarkRed'
+  uictx.strokeStyle = 'DarkRed'
+  uictx.fillText("Loading...", uiCanvas.width/2, uiCanvas.height/2)
+  uictx.stroke()
 }
 
 function drawStart(){
@@ -18,18 +74,22 @@ function drawStart(){
 
 
 
-export let game = {
-  loading : true,
-  state : 1, //game states: 1 means it hasn't started, 2 is mid-game, and 3 is post-game
-  loads: 6, // loads = 
-  
-}
+
 
 function updateGame(){
+  debugger;
+  if (game.loads == 0){
+    game.loading = false
+  }
   if (game.loading = true){
     drawLoad()
   } else
     if(game.state == 1){
       drawStart()
     }
+  else{
+    uictx.clearRect(0,0,uiCanvas.width,uiCanvas.height)
+  }
+  requestAnimationFrame(updateGame)
 }
+updateGame()
