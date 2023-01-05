3014b8fcb2b7c8a9290df9ee7d2aa5c4fab317c5
Etanjo <99751108+Etanjo@users.noreply.github.com>
2023-01-05 15:58:17 +0000

    added confirmation for successfully score submit

--- src/secretApp/scoreSetting.ts
+++ src/secretApp/scoreSetting.ts
@@ -2,12 +2,17 @@ export let submit = document.querySelector('#submit')
 import { lastScore } from "../../earthan/logistics"
 export let text = document.querySelector('#nameBox')
 let confirm = document.querySelector('#confirmation')
+function clearElement(element){
+element.innerHTML = ` `
+}
 submit?.addEventListener("click", async function(){
     console.log('adding score')
     let name = text.value
     try{
 var response = await fetch(`/.netlify/functions/setScore?name=${name}&score=${lastScore}`)
-if(response){confirm.innerHTML = `Confirmed!`}else{confirm.innerHTML =`Error`}
+if(response){confirm.innerHTML = `Confirmed!`
+setTimeout(clearElement, 1500, confirm)
+}else{confirm.innerHTML =`Error`}
     }
  catch(err){
     console.log(`
